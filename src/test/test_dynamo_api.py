13fe837bcf6d4ade014e1edef7f313f6e28ccbf8
Oishi.Yute <oishi.y@itix-system.co.jp>
2024-03-02 23:19:32 +0900

    Refactor variable names

--- src/test/test_dynamo_api.py
+++ src/test/test_dynamo_api.py
@@ -5,23 +5,24 @@ from moto import mock_aws
 import boto3
 
 sys.path.append("../")
-import dynamo_api
+from dynamo_api import *
 
 TABLE_DEF_FILE = "./test_data/table_def.json"
 TABLE_DATA_FILE = "./test_data/table_data.json"
+_table_name = "test_table"
 
 
 @pytest.fixture(autouse=True, scope="module")
 def setup_ddb():
     with mock_aws():
-        table_name = "foo"
+
         dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
 
         # テーブルの作成
         with open(TABLE_DEF_FILE) as f:
             foo_table_def = json.load(f)
         dynamodb.create_table(
-            TableName=table_name,
+            TableName=_table_name,
             **foo_table_def,
         )
 
@@ -29,12 +30,12 @@ def setup_ddb():
         with open(TABLE_DATA_FILE) as f:
             data_str = f.read()
         data_items = json.loads(data_str)
-        table = dynamodb.Table(table_name)
+        table = dynamodb.Table(_table_name)
         for item in data_items:
             table.put_item(Item=item)
         yield
 
 
 def test_get_item_from_id():
-    ret = dynamo_api.get_item_from_id("foo", "1")["data"]
-    assert ret == "data_01"
+    res = get_item_from_id(_table_name, "1")
+    assert res["data"] == "data_01"
